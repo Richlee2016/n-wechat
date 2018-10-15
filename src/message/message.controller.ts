f12b5e4739d9c350f23eff4324e689a4600e8859
Richlee2016 <345642459@qq.com>
2018-10-15 18:03:16 +0800

    处理微信消息

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,13 +1,17 @@
-import { Post, Controller, Query } from '@nestjs/common'
+import { Post, Controller, Req, Query } from '@nestjs/common'
 import { MessageService } from './message.service'
-
+import * as getRawBody from 'raw-body'
+import contentType from 'content-type'
 @Controller('')
 export class MessageController {
   constructor(private readonly Message: MessageService) {}
 
   @Post('wechat-hear')
-  async root(@Query() query) {
-    const Message = query
-    await this.Message.HandleMessage(Message)
+  async root(@Query() query, @Req() req) {
+    const message = await getRawBody(req, {
+      length: req.headers['content-length'],
+      limit: '1mb'
+    })
+    await this.Message.HandleMessage(query, message)
   }
 }
--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, Inject, HttpService } from '@nestjs/common'
 import { MessagePost } from './interfaces/message.interface'
 import Cof from '../../config'
-import sha from 'sha1'
+// import * as sha from 'sha1'
+var sha = require('sha1')
+import * as utils from './utils/utils'
 
 class Config {
   public wx: any = Cof.WXCOF
@@ -11,10 +13,16 @@ class Config {
 export class MessageService {
   private readonly Config: Config = new Config()
 
-  public async HandleMessage(Message: MessagePost) {
-    const { signature, timestamp, nonce, echostr } = Message
-    const str = [this.Config.wx.Token, timestamp, nonce].sort().join("");
-    const s = sha(str).toString();
+  public async HandleMessage(query: MessagePost, message) {
+    const { signature, timestamp, nonce, echostr } = query
+    const str = [this.Config.wx.Token, timestamp, nonce].sort().join('')
+    const s = sha(str).toString()
+    if (s !== signature) {
+      return false
+    } else {
+      const msgRes = await utils.parmsXml(message)
+      console.log(msgRes);
+    }
     return 'kl'
   }
 }
